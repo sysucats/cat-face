4007c99208a8b020d1895605f15cc33b54bba1d2
Jazon <47637319+jxzhn@users.noreply.github.com>
2022-05-27 17:23:17 +0800

    自动删除已存在的目标目录

--- data_preprocess.py
+++ data_preprocess.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 from yolo_interface import yolov5m
 from tqdm import tqdm
 from PIL import Image
@@ -13,20 +14,18 @@ def main():
     numPhotos = 0
     numSkippedPhotos = 0
 
-    if not os.path.exists(DEST):
-        os.mkdir(DEST)
-    else:
-        print('### destination path exists, continue...')
+    if os.path.exists(DEST):
+        shutil.rmtree(DEST)
+    os.mkdir(DEST)
 
     print("==> processing photos...")
     for dirName in tqdm(os.listdir(SRC), leave=False, desc="processing"):
         srcPath = os.path.join(SRC, dirName)
-        if os.path.isdir(srcPath) == False:
+        if not os.path.isdir(srcPath):
             continue
 
         destPath = os.path.join(DEST, dirName)
-        if not os.path.isdir(destPath):
-            os.mkdir(destPath)
+        os.mkdir(destPath)
         
         for fileName in tqdm(os.listdir(srcPath), leave=False, desc=dirName):
             numPhotos += 1
@@ -44,10 +43,7 @@ def main():
             # 裁剪出cat
             catResult = catResults[0]
             cropBox = catResult['xmin'], catResult['ymin'], catResult['xmax'], catResult['ymax']
-            im = Image.open(srcFilePath).crop(cropBox)
-            if im.mode in ("RGBA", 'P'):
-                im = im.convert('RGB')
-            im.save(destFilePath, format='JPEG')
+            Image.open(srcFilePath).convert('RGB').crop(cropBox).save(destFilePath, format='JPEG')
     
     print(f"==> done. {numPhotos - numSkippedPhotos} photos cropped, {numSkippedPhotos} photos skipped.")
 
